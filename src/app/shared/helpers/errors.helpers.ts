881bb2be8b95a1378fec7a1c00e86bef30a40e26
2PaeDev <2paedev@gmail.com>
2022-03-29 17:49:26 +0200

    fix error helper

--- src/app/shared/helpers/errors.helpers.ts
+++ src/app/shared/helpers/errors.helpers.ts
@@ -1,21 +1,22 @@
 import { AbstractControl } from '@angular/forms';
 import { ERROR_TYPES } from '../const/errors.const';
 
-export function hasError(
+export const hasError = (
   formControl: AbstractControl,
-  errorType: string
-): boolean {
+  errorType: string,
+  submitted: boolean
+): boolean => {
   if (errorType === ERROR_TYPES.required) {
-    return this.submitted && formControl.errors?.required;
+    return submitted && formControl.errors?.required;
   }
 
   if (errorType === ERROR_TYPES.email) {
-    return this.submitted && formControl.errors?.email;
+    return submitted && formControl.errors?.email;
   }
 
   if (errorType === ERROR_TYPES.minLength) {
-    return this.submitted && formControl.errors?.minlength;
+    return submitted && formControl.errors?.minlength;
   }
 
   return false;
-}
+};
